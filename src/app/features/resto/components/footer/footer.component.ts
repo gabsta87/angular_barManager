6010c9044bfbd0f797181db4f1c1badd3f7435c8
gabsta87 <gabrielmaretsecondaire@gmail.com>
2022-08-02 14:57:32 +0200

    access to angularfire denied

--- src/app/features/resto/components/footer/footer.component.ts
+++ src/app/features/resto/components/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { collectionData, deleteDoc, doc, Firestore, query, setDoc, updateDoc } from '@angular/fire/firestore';
+import { collection, DocumentData } from '@firebase/firestore';
+import { Observable } from 'rxjs';
+import { DbaccessService } from '../../services/dbaccess.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,11 +11,48 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent{
 
+  // constructor(private readonly _db: DbaccessService,private readonly _db: Firestore){
+  constructor(private readonly _db: Firestore){}
+
   billValue : number = 0;
 
   now : Date = new Date();
 
   updateBill(param:number){
     this.billValue = param;
+  }
+
+  // sendOrder(){
+  //   this._db.addOrder(this.billValue);
+  // }
+
+  _myData!:Observable<DocumentData[]>;
+  _dbName:string = "resto";
+
+  getOrders(){
+    const myCollection = collection(this._db,this._dbName);
+    const q = query(myCollection);
+    this._myData = collectionData(q,{idField:'id'});
+  }
+
+  async addOrder(newValue:number){
+    const id = Date.now();
+    const docRef = doc(this._db,this._dbName+'/'+id);
+    debugger;
+    console.log("docRef : ",docRef);
+    await setDoc(docRef,{orderValue:newValue}).catch(err => {
+      console.log("Error : ",err);
+    });
+    console.log("value written");
+  }
+
+  async updateOrder(id:string){
+    const docRef = doc(this._db, `${this._dbName}/${id}`);
+    updateDoc(docRef,{done:true});
+  }
+
+  async deleteOrder(id:string){
+    const docRef = doc(this._db,`${this._dbName}/${id}`);
+    deleteDoc(docRef);
   }
 }
--- src/app/features/resto/services/dbaccess.service.ts
+++ src/app/features/resto/services/dbaccess.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { collectionData, Firestore, query, DocumentData, doc, setDoc, updateDoc, deleteDoc } from '@angular/fire/firestore';
+import { collection } from '@firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DbaccessService {
+export class DbaccessService{
 
-  constructor() { }
+  private _myData!:Observable<DocumentData[]>;
+  private _dbName:string = "resto";
+
+  constructor(private readonly _db: Firestore) {
+    const myCollection = collection(this._db,this._dbName);
+    const q = query(myCollection);
+    this._myData = collectionData(q,{idField:'id'});
+  }
+
+  getOrders(){
+    return this._myData;
+  }
+
+  addOrder(newValue:number){
+    const id = Date.now();
+    const docRef = doc(this._db,this._dbName+'/'+id);
+    console.log("docRef = ",docRef);
+    setDoc(docRef,{orderValue:newValue});
+  }
+
+  async updateOrder(id:string){
+    const docRef = doc(this._db, `${this._dbName}/${id}`);
+    updateDoc(docRef,{done:true});
+  }
+
+  async deleteOrder(id:string){
+    const docRef = doc(this._db,`${this._dbName}/${id}`);
+    deleteDoc(docRef);
+  }
 }
